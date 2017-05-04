4753d724cbbb1a63aed526a7e570aecbc74af385
Shane Lacey <shanelacey@shanes-mac.local>
2017-05-04 16:36:15 +0100

    Implemented a slider on view, no function yet

--- EtchASketch/ViewController.swift
+++ EtchASketch/ViewController.swift
@@ -13,6 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        showSlider()
+    }
+    
+    func showSlider() {
+        let sliderHorizontal = CircleSlider(frame: CGRect(x:self.view.frame.width - 125, y:self.view.frame.height - 125, width:125, height:125))
+        sliderHorizontal.circle_color = UIColor.white
+        sliderHorizontal.touch_color = UIColor.blue
+        
+        sliderHorizontal.setSelector(forAction: .touchMoved, target: self, selector: #selector(movedSlider(_:)))
+        
+        sliderHorizontal.makeSlider()
+        self.view.addSubview(sliderHorizontal)
+        print("Width:" , self.view.frame.width)
+        print("Height:" , self.view.frame.height)
+    }
+    
+    func movedSlider(_ rad: AnyObject) {
+        print("Moved slider:", rad)
     }
 
     override func didReceiveMemoryWarning() {
