c51de7f50dd9c1de25bc6ba8437d8a15cf4bdb48
20013687 <20013687@it103imac05.wit.ie>
2017-05-17 13:19:41 +0100

    Added permission to access Camera Roll. Added 'Save Drawing' functionality, needs some fixing to remove the optionView from it but it works for now.

--- EtchASketch/ViewController.swift
+++ EtchASketch/ViewController.swift
@@ -139,7 +139,14 @@ class ViewController: UIViewController {
     }
     
     @IBAction func saveDrawing(_ sender: AnyObject) {
-        hideOptionView(self)
+//        var screenshot = self.view.snapshotView(afterScreenUpdates: true)
+//        var screenshot = DrawView.snapshotView(afterScreenUpdates: true)
+        let bounds = self.DrawView.layer.bounds
+        UIGraphicsBeginImageContextWithOptions(bounds.size, true, UIScreen.main.scale)
+        self.view.drawHierarchy(in: bounds, afterScreenUpdates: true)
+        let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+        UIImageWriteToSavedPhotosAlbum(screenshot!, nil, nil, nil)
+//        hideOptionView(self)
     }
     
     @IBAction func shareDrawing(_ sender: AnyObject) {
